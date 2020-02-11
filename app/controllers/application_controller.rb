2b28818614cf888138ae8b1de3744fe6b16d73bc
Jeff Swisher <jefftswisher@gmail.com>
2020-02-11 22:17:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,11 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		user = User.new(username: params[:username], password: params[:password])
-		
-		if user.save 
-		  redirect "/login"
-		else 
-		  redirect "/failure"
+		user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
+			redirect "/login"
+		else
+			redirect "/failure"
 		end
 	end
 
@@ -31,15 +30,13 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		user = User.new(username: pararms[:username])
-		
-  	if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/success"
-    else
-      redirect "/failure"
-    end
-		
+		user = User.find_by(username: params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/success"
+		else
+			redirect "failure"
+		end
 	end
 
 	get "/success" do
@@ -61,11 +58,11 @@ class ApplicationController < Sinatra::Base
 
 	helpers do
 		def logged_in?
-			!!session[:id]
+			!!session[:user_id]
 		end
 
 		def current_user
-			User.find(session[:id])
+			User.find(session[:user_id])
 		end
 	end
 
